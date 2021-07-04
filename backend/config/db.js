baa775dd4564f38e9d5e8c1b240913d5e3514bf0
jgwynn <46574659+jgwynn@users.noreply.github.com>
2021-07-04 12:03:03 -0400

    test commit

---
+++ backend/config/db.js
@@ -0,0 +1,22 @@
+
+import mongoose from 'mongoose'
+import User from '../models/usersModel.js'
+
+const connectDB = async () => {
+    try {
+        //database Name
+        const databaseName='TestRun';
+        console.log(`attempting connection`)
+        const con = await mongoose.connect(`mongodb://127.0.0.1:27017/${databaseName}`, { 
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    });
+        console.log(`Database connected : ${con.connection.host}`)
+    } catch (error) {
+        console.error(`Error: ${error.message}`)
+        process.exit(1)
+    }
+}
+
+export default connectDB
