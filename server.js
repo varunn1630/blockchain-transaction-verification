4b3984332a2f52d215a24e26a5f1ae1261a86f5e
jgwynn <46574659+jgwynn@users.noreply.github.com>
2021-09-01 13:52:49 -0400

    prep for heroku deployment
    express.static is in charge of sending static files requests to the client. So when the browser requests logo.png from your site, it knows to look in the build folder for that.
    
    app.get("*") is a "catchall" route handler. It needs to be near the bottom of your server file so that it will only be enacted if the API routes above it don't handle the request. It's in charge of sending the main index.html file back to the client if it didn't receive a request it recognized otherwise.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import userRoutes from './routes/userRoute.js'
 import express from 'express'
 import dotenv  from 'dotenv'
 import cors from 'cors'
+import path from 'path'
 
 const PORT = process.env.PORT || 5000
 
@@ -19,7 +20,14 @@ app.use('/api/users', userRoutes)
 
 app.use(cors())
 
+// ... other app.use middleware 
+app.use(express.static(path.join(__dirname, "client", "build")))
 
+
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 //Express js listen method to run project on http://localhost:5000
 app.listen(PORT, console.log(`App is running in ${process.env.NODE_ENV} mode on port ${PORT}`))
