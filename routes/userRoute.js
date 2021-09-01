5ad60c56ef567bacd4c70a11afd5ec66b6b7670d
jgwynn <46574659+jgwynn@users.noreply.github.com>
2021-09-01 14:23:11 -0400

    add delete + function renames
    login and register now have user attached at the end to be more descriptive

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,9 +6,12 @@ const router = express.Router()
 
 
 // express router method to create route for registering users
-router.route('/register').get(register)
+router.route('/register').get(registerUser)
 
 // express router method to create route for logging in users
-router.route('/login').get(login)
+router.route('/login').get(loginUser)
+
+// express router method to create route for deleting users
+router.route('/delete/:id').get(deleteUser)
 
 export default router
