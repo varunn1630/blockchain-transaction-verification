bbde30628fbff9cd5976b890784dffa11aecf0ab
jgwynn <46574659+jgwynn@users.noreply.github.com>
2021-09-13 18:22:29 -0400

    Update supplierRoute.js

--- routes/supplierRoute.js
+++ routes/supplierRoute.js
@@ -1,1 +1,19 @@
+//import { registerUser, loginUser, deleteUser, verifyUser} from "../controllers/userController.js";
+import express from 'express'
 
+import Supply from '../models/supplierModel.js'
+const supplyRouter = express.Router()
+
+
+
+// express router method to create route for registering users
+//supplyRouter.route('/register').post(registerUser)
+
+// express router method to create route for logging in users
+//supplyRouter.route('/login').post(loginUser)
+
+// express router method to create route for deleting users
+//supplyRouter.route('/delete/:id').delete(deleteUser)
+
+
+export default supplyRouter
